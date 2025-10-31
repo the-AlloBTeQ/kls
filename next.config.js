ee20d95a9a11a4de41308df365a117c1b0f88881
BMMvelase1912 <111530553+BMMvelase1912@users.noreply.github.com>
2025-10-31 12:55:35 +0200

    storm

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  distDir: 'build',
-  reactStrictMode: false,
-}
+const nextConfig = {}
 
 module.exports = nextConfig
